85e8735d805c34849869249675d58e7eeef510ff
lRustyShackleford <linares.damian@gmail.com>
2022-04-26 21:50:06 -0700

    added images to render

--- src/App.js
+++ src/App.js
@@ -5,35 +5,47 @@ const App = () => {
     {
       id: 1,
       title: 'Hats',
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+      description: "Wearing a hat",
     },
     {
       id: 2,
       title: 'Jackets',
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+      description: "Variety of jackets"
     },
     {
       id: 3,
       title: 'Sneakers',
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+      description: "Variety of sneakers"
     },
     {
       id: 4,
-      title: 'Mens',
+      title: 'Womens',
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+      description: "Woman posing"
     },
     {
       id: 5,
-      title: 'Womens',
+      title: 'Mens',
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+      description: "Man posing"
     },
   ];
 
   return (
     <div className="categories-container">     
-          {categories.map((category) => {
+          {categories.map(({id, title, imageUrl}) => {
             return (
-              <div className="category-container">
-                <div className="background-image">
-                  {/*<img />*/}
+              <div key={id} className="category-container">
+                <div className="background-image" style={{
+                  backgroundImage: `url(${imageUrl})`
+                }}>
+                  
                 </div>
-                <div className="category-body-container" key={category.id}>
-                  <h2>{category.title}</h2>
+                <div className="category-body-container">
+                  <h2>{title}</h2>
                   <p>Shop Now</p>
                 </div>
               </div>
