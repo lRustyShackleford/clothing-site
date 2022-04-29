f76d374016eeb38781e0034ac0833fa3f8acb29b
lRustyShackleford <linares.damian@gmail.com>
2022-04-28 19:12:10 -0700

    add nav routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './categories.styles.scss';
-import CategoryDirectory from './components/category-directory/category-directory.component';
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import { Routes, Route} from "react-router-dom";
+
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>
+}
 
 const App = () => {
-  return (     
-    <CategoryDirectory />
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
+    </Routes> 
   );
 }
 
